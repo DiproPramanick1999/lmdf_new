95f6ec8e915a061a09d2d4713dc5df23b022be60
DiproPramanick1999 <43265260+DiproPramanick1999@users.noreply.github.com>
2020-04-30 09:04:55 +0530

    30-04-2020#D3

--- application/views/back/book.php
+++ application/views/back/book.php
@@ -6,320 +6,121 @@
    <div class="container-fluid">
      <div class="row mb-2">
        <div class="col-sm-6">
-         <h1>Book A Appointment</h1>
+         <h1>Book An Appointment</h1>
        </div>
        <div class="col-sm-6">
          <ol class="breadcrumb float-sm-right">
            <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Home</a></li>
-           <li class="breadcrumb-item active">book appt</li>
+           <li class="breadcrumb-item active">Book appointment</li>
          </ol>
        </div>
      </div>
    </div><!-- /.container-fluid -->
  </section>
-
+<!--===============================================================SLOT BOOKING========================================================-->
  <section class="content">
    <div class="container-fluid">
      <div class="row">
          <div class="col-md-10">
-           <!-- general form elements disabled -->
            <div class="card card-warning">
              <div class="card-header">
-               <h3 class="card-title">General Elements</h3>
+               <h3 class="card-title">SLOT BOOKING</h3>
              </div>
              <!-- /.card-header -->
              <div class="card-body">
-               <form role="form">
-                 <div class="row">
-                   <div class="col-sm-6">
-                     <!-- text input -->
-                     <div class="form-group">
-                       <label>Text</label>
-                       <input type="text" class="form-control" placeholder="Enter ...">
-                     </div>
-                   </div>
-                   <div class="col-sm-6">
-                     <div class="form-group">
-                       <label>Text Disabled</label>
-                       <input type="text" class="form-control" placeholder="Enter ..." disabled>
-                     </div>
-                   </div>
-                 </div>
-                 <div class="row">
-                   <div class="col-sm-6">
-                     <!-- textarea -->
-                     <div class="form-group">
-                       <label>Textarea</label>
-                       <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                     </div>
-                   </div>
-                   <div class="col-sm-6">
-                     <div class="form-group">
-                       <label>Textarea Disabled</label>
-                       <textarea class="form-control" rows="3" placeholder="Enter ..." disabled></textarea>
-                     </div>
-                   </div>
-                 </div>
+               <form role="form" method="post" action="<?php echo base_url()?>book_l/booking">
+               <?php
+                   if($this->uri->segment(2) == 'inserted'){
+                       echo '<p class="text-success">Your Slot has been booked successfully</p>';
+                   }
+                   ?>
 
-                 <!-- input states -->
-                 <div class="form-group">
-                   <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i> Input with
-                     success</label>
-                   <input type="text" class="form-control is-valid" id="inputSuccess" placeholder="Enter ...">
-                 </div>
-                 <div class="form-group">
-                   <label class="col-form-label" for="inputWarning"><i class="far fa-bell"></i> Input with
-                     warning</label>
-                   <input type="text" class="form-control is-warning" id="inputWarning" placeholder="Enter ...">
-                 </div>
-                 <div class="form-group">
-                   <label class="col-form-label" for="inputError"><i class="far fa-times-circle"></i> Input with
-                     error</label>
-                   <input type="text" class="form-control is-invalid" id="inputError" placeholder="Enter ...">
-                 </div>
-
-                 <div class="row">
-                   <div class="col-sm-6">
-                     <!-- checkbox -->
-                     <div class="form-group">
-                       <div class="form-check">
-                         <input class="form-check-input" type="checkbox">
-                         <label class="form-check-label">Checkbox</label>
-                       </div>
-                       <div class="form-check">
-                         <input class="form-check-input" type="checkbox" checked>
-                         <label class="form-check-label">Checkbox checked</label>
-                       </div>
-                       <div class="form-check">
-                         <input class="form-check-input" type="checkbox" disabled>
-                         <label class="form-check-label">Checkbox disabled</label>
-                       </div>
-                     </div>
-                   </div>
-                   <div class="col-sm-6">
-                     <!-- radio -->
-                     <div class="form-group">
-                       <div class="form-check">
-                         <input class="form-check-input" type="radio" name="radio1">
-                         <label class="form-check-label">Radio</label>
-                       </div>
-                       <div class="form-check">
-                         <input class="form-check-input" type="radio" name="radio1" checked>
-                         <label class="form-check-label">Radio checked</label>
-                       </div>
-                       <div class="form-check">
-                         <input class="form-check-input" type="radio" disabled>
-                         <label class="form-check-label">Radio disabled</label>
-                       </div>
-                     </div>
-                   </div>
-                 </div>
-
+                <?php date_default_timezone_set("Asia/Calcutta");
+                      $today = date('Y-m-d'); 
+                      $tomorrow = date("Y-m-d", strtotime("+1 day"));
+                      $this->session->userdata('userid');
+                 ?>
                  <div class="row">
                    <div class="col-sm-6">
                      <!-- select -->
                      <div class="form-group">
-                       <label>Select</label>
-                       <select class="form-control">
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
+                       <label>Day Available</label>
+                       <select class="form-control" name="date" id="sel_date" onchange="checker()">
+                         <option value="<?php echo $today;?>">Today</option>
+                         <option value="<?php echo $tomorrow;?>">Tomorrow</option>
                        </select>
                      </div>
                    </div>
                    <div class="col-sm-6">
                      <div class="form-group">
-                       <label>Select Disabled</label>
-                       <select class="form-control" disabled>
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
+                       <label>Time Available</label>
+                       <select class="form-control" name="time" id="sel_time" onchange="checker()">
+                         <option value="5">5:00-6:00</option>
+                         <option value="6">6:30-7:30</option>
+                         <option value="8">8:00-9:00</option>
+                         <option value="9">9:30-10:30</option>
+                         <option value="11">11:00-12:00</option>
+                         <option value="12">12:30-13:30</option>
+                         <option value="14">14:00-15:00</option>
+                         <option value="15">15:30-16:30</option>
+                         <option value="17">17:00-18:00</option>
+                         <option value="18">18:30-19:30</option>
+                         <option value="20">20:00-21:00</option>
+                         <option value="21">21:30-22:30</option>
                        </select>
                      </div>
                    </div>
                  </div>
+                 
 
-                 <div class="row">
-                   <div class="col-sm-6">
-                     <!-- Select multiple-->
-                     <div class="form-group">
-                       <label>Select Multiple</label>
-                       <select multiple class="form-control">
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
-                       </select>
-                     </div>
-                   </div>
-                   <div class="col-sm-6">
-                     <div class="form-group">
-                       <label>Select Multiple Disabled</label>
-                       <select multiple class="form-control" disabled>
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
-                       </select>
-                     </div>
-                   </div>
-                 </div>
+
+              <div class="card-footer">
+                  <button type="submit" name="insert" value="Insert" id="buttn" class="btn btn-info float-right btn-block">Book Slot</button>
+                </div>
                </form>
              </div>
              <!-- /.card-body -->
            </div>
-           <!-- /.card -->
-           <!-- general form elements disabled -->
-           <div class="card card-secondary">
-             <div class="card-header">
-               <h3 class="card-title">Custom Elements</h3>
-             </div>
-             <!-- /.card-header -->
-             <div class="card-body">
-               <form role="form">
-                 <div class="row">
-                   <div class="col-sm-6">
-                     <!-- checkbox -->
-                     <div class="form-group">
-                       <div class="custom-control custom-checkbox">
-                         <input class="custom-control-input" type="checkbox" id="customCheckbox1" value="option1">
-                         <label for="customCheckbox1" class="custom-control-label">Custom Checkbox</label>
-                       </div>
-                       <div class="custom-control custom-checkbox">
-                         <input class="custom-control-input" type="checkbox" id="customCheckbox2" checked>
-                         <label for="customCheckbox2" class="custom-control-label">Custom Checkbox checked</label>
-                       </div>
-                       <div class="custom-control custom-checkbox">
-                         <input class="custom-control-input" type="checkbox" id="customCheckbox3" disabled>
-                         <label for="customCheckbox3" class="custom-control-label">Custom Checkbox disabled</label>
-                       </div>
-                     </div>
-                   </div>
-                   <div class="col-sm-6">
-                     <!-- radio -->
-                     <div class="form-group">
-                       <div class="custom-control custom-radio">
-                         <input class="custom-control-input" type="radio" id="customRadio1" name="customRadio">
-                         <label for="customRadio1" class="custom-control-label">Custom Radio</label>
-                       </div>
-                       <div class="custom-control custom-radio">
-                         <input class="custom-control-input" type="radio" id="customRadio2" name="customRadio" checked>
-                         <label for="customRadio2" class="custom-control-label">Custom Radio checked</label>
-                       </div>
-                       <div class="custom-control custom-radio">
-                         <input class="custom-control-input" type="radio" id="customRadio3" disabled>
-                         <label for="customRadio3" class="custom-control-label">Custom Radio disabled</label>
-                       </div>
-                     </div>
-                   </div>
-                 </div>
-
-                 <div class="row">
-                   <div class="col-sm-6">
-                     <!-- select -->
-                     <div class="form-group">
-                       <label>Custom Select</label>
-                       <select class="custom-select">
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
-                       </select>
-                     </div>
-                   </div>
-                   <div class="col-sm-6">
-                     <div class="form-group">
-                       <label>Custom Select Disabled</label>
-                       <select class="custom-select" disabled>
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
-                       </select>
-                     </div>
-                   </div>
-                 </div>
-
-                 <div class="row">
-                   <div class="col-sm-6">
-                     <!-- Select multiple-->
-                     <div class="form-group">
-                       <label>Custom Select Multiple</label>
-                       <select multiple class="custom-select">
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
-                       </select>
-                     </div>
-                   </div>
-                   <div class="col-sm-6">
-                     <div class="form-group">
-                       <label>Custom Select Multiple Disabled</label>
-                       <select multiple class="custom-select" disabled>
-                         <option>option 1</option>
-                         <option>option 2</option>
-                         <option>option 3</option>
-                         <option>option 4</option>
-                         <option>option 5</option>
-                       </select>
-                     </div>
-                   </div>
-                 </div>
-
-                 <div class="form-group">
-                   <div class="custom-control custom-switch">
-                     <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                     <label class="custom-control-label" for="customSwitch1">Toggle this custom switch element</label>
-                   </div>
-                 </div>
-                 <div class="form-group">
-                   <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
-                     <input type="checkbox" class="custom-control-input" id="customSwitch3">
-                     <label class="custom-control-label" for="customSwitch3">Toggle this custom switch element with custom colors danger/success</label>
-                   </div>
-                 </div>
-                 <div class="form-group">
-                   <div class="custom-control custom-switch">
-                     <input type="checkbox" class="custom-control-input" disabled id="customSwitch2">
-                     <label class="custom-control-label" for="customSwitch2">Disabled custom switch element</label>
-                   </div>
-                 </div>
-                 <div class="form-group">
-                   <label for="customRange1">Custom range</label>
-                   <input type="range" class="custom-range" id="customRange1">
-                 </div>
-                 <div class="form-group">
-                   <label for="customRange1">Custom range (custom-range-danger)</label>
-                   <input type="range" class="custom-range custom-range-danger" id="customRange1">
-                 </div>
-                 <div class="form-group">
-                   <label for="customRange1">Custom range (custom-range-teal)</label>
-                   <input type="range" class="custom-range custom-range-teal" id="customRange1">
-                 </div>
-                 <div class="form-group">
-                   <!-- <label for="customFile">Custom File</label> -->
-
-                   <div class="custom-file">
-                     <input type="file" class="custom-file-input" id="customFile">
-                     <label class="custom-file-label" for="customFile">Choose file</label>
-                   </div>
-                 </div>
-                 <div class="form-group">
-                 </div>
-               </form>
-             </div>
-             <!-- /.card-body -->
-           </div>
+           <script>
+                     console.log("hello");
+                     function checker(){
+                         console.log("hello");
+                         $.ajax({
+                             
+                             type : 'POST',
+                             url : '<?php echo base_url();?>book_l/checkAvailability',
+                             data : {
+                                 'date' : $("#sel_date").val(),
+                                 'time' : $("#sel_time").val(),
+                                 'id' : <?php echo $user['userid'];?>
+                             },
+                             success: function(msg){
+                                 //alert(msg);
+                                 if(msg!= 'Success'){
+                                     console.log(msg);
+                                     $("#buttn").attr('disabled',  "disabled");
+                                     //alert("he");
+                                     //alert(msg);
+                                 }
+                                 else{
+                                     //
+                                     console.log(msg);
+                                     //alert('she');
+                                    // alert("YYEESS");
+                                     $("#buttn").removeAttr("disabled");
+                                 }
+                             },
+                             error: function(msg){
+                                 console.log(msg);
+                                 alert("server error try again later");
+                             }
+                             
+                         });
+                     }
+                            checker();
+                   
+                   </script>
+          
            <!-- /.card -->
          </div>
        </div>
